8ffe6542be97ee9f80d3ac3a7249f1e587b70561
rfreitas16 <rfreitas16@hotmail.com>
2025-09-05 14:41:07 -0300

    criando component header responsivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
+import { Container } from '@/components/container';
+import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostList';
 import { SpinsLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div
-      className='text-slate-900 bg-slate-100 min-h-screen
-     dark:bg-slate-900 dark:text-slate-100'
-    >
+    <Container>
+      <Header />
       <Suspense fallback={<SpinsLoader />}>
         <PostsList />
       </Suspense>
-    </div>
+    </Container>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,19 @@
-'use client';
+import clsx from 'clsx';
 
-export function Header(){
-  console.log('header')
-  return(
-    <h1>Ola mundo</h1>
-  )
+export function Header() {
+  return (
+    <header>
+      <h1
+        className={clsx(
+          'text-4xl/normal font-extrabold py-8',
+          'sm:text-5xl/normal sm:py-10',
+          'md:text-6xl/normal md:py-11',
+          'lg:text-7xl/normal lg:py-12',
+        )}
+      >
+        Ola mundo
+      </h1>
+      <a href='#'>the blog</a>
+    </header>
+  );
 }

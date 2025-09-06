0d74dee1136ed3c9b1c32f75e0e5c0b72883cfbe
rfreitas16 <rfreitas16@hotmail.com>
2025-09-06 14:56:03 -0300

    component dos posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Container } from '@/components/container';
 import { Header } from '@/components/Header';
+import { PostCoverImage } from '@/components/PostCoverImage';
 import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostList';
 import { SpinsLoader } from '@/components/SpinLoader';
-import Image from 'next/image';
-import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
@@ -16,19 +15,26 @@ export default async function HomePage() {
         className='grid grid-cols-1 gap-8 mb-16
       sm:grid-cols-2 group'
       >
-        <Link href='#' className='w-full h-full overflow-hidden'>
-          <Image
-            className='w-full h-full object-cover object-center group-hover:scale-105 transition rounded-xl'
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt='titulo do post'
-            priority
-          ></Image>
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: '/posts/asisajas',
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: '/images/bryen_9.png',
+            alt: 'alt da imagem',
+            priority: true,
+          }}
+        />
+
         <div className='flex flex-col gap-4 sm:justify-center'>
-          <time  className='text-slate-600 text-sm/tight' dateTime='2025-04-20'>20/04/2025 10:00</time>
-        <PostHeading as='h1' url='#'>em ipsum dolor si</PostHeading>
+          <time className='text-slate-600 text-sm/tight' dateTime='2025-04-20'>
+            20/04/2025 10:00
+          </time>
+          <PostHeading as='h1' url='#'>
+            em ipsum dolor si
+          </PostHeading>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
             fugiat esse molestias, similique voluptatibus necessitatibus

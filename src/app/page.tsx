00ece2b58c43a2329f466df6a54cde6d1cf855b4
rfreitas16 <rfreitas16@hotmail.com>
2025-09-08 15:25:19 -0300

    titulos da pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
-import { Container } from '@/components/container';
-import { Header } from '@/components/Header';
-import { PostCoverImage } from '@/components/PostCoverImage';
 import { PostFeatured } from '@/components/PostFeatured';
-import { PostHeading } from '@/components/PostHeading';
+
 import { PostsList } from '@/components/PostList';
 import { SpinsLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinsLoader />}>
         <PostFeatured />
       </Suspense>
       <Suspense fallback={<SpinsLoader />}>
         <PostsList />
       </Suspense>
-    </Container>
+    </>
   );
 }

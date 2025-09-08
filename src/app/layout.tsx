e037e3c03bab1477ab7a4e3966b064e58a423f14
rfreitas16 <rfreitas16@hotmail.com>
2025-09-08 15:42:43 -0300

    component footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Container } from '@/components/container';
 import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: {
@@ -23,6 +24,8 @@ export default function RootLayout({
           <Header />
 
           {children}
+
+          <Footer></Footer>
         </Container>
       </body>
     </html>

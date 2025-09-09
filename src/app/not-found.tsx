9a5b5286c8bec06823859475fa203aec824a2207
rfreitas16 <rfreitas16@hotmail.com>
2025-09-09 13:10:35 -0300

    pagina not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+export default function NotFoundPage() {
+  return (
+    <div
+      className={clsx(
+        'min-h-[320px] bg-slate-900 text-slate-100',
+        'mb-16 p-8 rounded-xl',
+        'flex items-center justify-center',
+        'text-center',
+      )}
+    >
+      <div>
+        <h1 className='text-7xl/tight mb-8 font-extrabold'>Erro 404</h1>
+        <p>A pagina que voce esta tentando acessar nao existe</p>
+      </div>
+    </div>
+  );
+}

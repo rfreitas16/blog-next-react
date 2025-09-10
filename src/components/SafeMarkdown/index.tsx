92fdc5bcd79ec2fc4237ede90fa7e1791141f692
rfreitas16 <rfreitas16@hotmail.com>
2025-09-10 14:36:47 -0300

    colocando tailwind typography

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import ReactMarkdown from 'react-markdown';
 import rehypeSanitize from 'rehype-sanitize';
 import remarkGfm from 'remark-gfm';
@@ -6,7 +7,21 @@ type SafeMarkdownProps = {
 };
 export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
   return (
-    <div>
+    <div
+      className={clsx(
+        'prose-slate',
+        'w-full',
+        'max-w-none',
+        'overflow-hidden',
+        'prose-a:transition',
+        'prose-a:no-underline',
+        'prose-a:text-blue-500',
+        'prose-a:hover:text-blue-700',
+        'prose-a:hover:underline',
+        'prose-img:mx-auto',
+        'md:prose-lg',
+      )}
+    >
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}

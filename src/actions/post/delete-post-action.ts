6d72102855b2e240ec12043b1c571c354d25d506
rfreitas16 <rfreitas16@hotmail.com>
2025-09-16 15:05:07 -0300

    movendo botao para component e userclient

---
+++ src/actions/post/delete-post-action.ts
@@ -0,0 +1,7 @@
+import { logColor } from '@/utils/log-color';
+
+export async function deletePostAction(formData: FormData) {
+  'use server';
+  const id = formData.get('id');
+  logColor('' + id);
+}

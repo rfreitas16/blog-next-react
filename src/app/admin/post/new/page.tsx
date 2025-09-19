91a167784aab0a9c49eb67253bcb1ffcfeac08ec
rfreitas16 <rfreitas16@hotmail.com>
2025-09-19 14:31:38 -0300

    colocando markdown

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,28 +1,6 @@
-import { Button } from '@/components/Button';
-import { InputCheckbox } from '@/components/InputCheckbox/InputCheckbox';
-import { InputText } from '@/components/InputText/InputText';
+import { ManagePostForm } from '@/components/admin/ManagePostForm';
 
 export const dynamic = 'force-dynamic';
 export default async function AdminPostNewPage() {
-  return (
-    <form action='' className='mb-16'>
-      <div className='flex flex-col gap-6'>
-        <InputText labelText='nome' placeholder='Digite seu nome' />
-
-        <InputText labelText='sobrenome' placeholder='Digite seu sobrenome' />
-        <InputText
-          disabled
-          labelText='sobrenome'
-          placeholder='Digite seu sobrenome'
-        />
-        <InputText disabled labelText='destivado' placeholder='desativado' />
-        <InputText readOnly labelText='readonly' placeholder='readonly' />
-        <InputCheckbox labelText='Publicado'></InputCheckbox>
-
-        <div className='mt-4'>
-          <Button type='submit'>Enviar</Button>
-        </div>
-      </div>
-    </form>
-  );
+  return <ManagePostForm />;
 }

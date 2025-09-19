3f941dec95894f366abdedc4e8bfa7b2e8cc90f8
rfreitas16 <rfreitas16@hotmail.com>
2025-09-19 13:45:16 -0300

    criando input checkbox

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,19 +1,28 @@
+import { Button } from '@/components/Button';
+import { InputCheckbox } from '@/components/InputCheckbox/InputCheckbox';
 import { InputText } from '@/components/InputText/InputText';
 
 export const dynamic = 'force-dynamic';
 export default async function AdminPostNewPage() {
   return (
-    <div className='flex flex-col gap-6'>
-      <InputText labelText='nome' placeholder='Digite seu nome' />
+    <form action='' className='mb-16'>
+      <div className='flex flex-col gap-6'>
+        <InputText labelText='nome' placeholder='Digite seu nome' />
 
-      <InputText labelText='sobrenome' placeholder='Digite seu sobrenome' />
-      <InputText
-        disabled
-        labelText='sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-      <InputText disabled labelText='destivado' placeholder='desativado' />
-      <InputText readOnly labelText='readonly' placeholder='readonly' />
-    </div>
+        <InputText labelText='sobrenome' placeholder='Digite seu sobrenome' />
+        <InputText
+          disabled
+          labelText='sobrenome'
+          placeholder='Digite seu sobrenome'
+        />
+        <InputText disabled labelText='destivado' placeholder='desativado' />
+        <InputText readOnly labelText='readonly' placeholder='readonly' />
+        <InputCheckbox labelText='Publicado'></InputCheckbox>
+
+        <div className='mt-4'>
+          <Button type='submit'>Enviar</Button>
+        </div>
+      </div>
+    </form>
   );
 }

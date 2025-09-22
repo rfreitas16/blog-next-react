70100c8aabd6243057e04c5a7cb43678199d6ddf
rfreitas16 <rfreitas16@hotmail.com>
2025-09-22 13:54:16 -0300

    preenchendo o formulario pela bd

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,14 +1,32 @@
+import { ManagePostForm } from '@/components/admin/ManagePostForm';
+import { makePublicpost } from '@/dto/post/dto';
+import { findPostByIdAdmin } from '@/lib/post/queries/admin';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
 export const dynamic = 'force-dynamic';
+export const metadata: Metadata = {
+  title: 'Editar post',
+};
 
 type AdminPostIdPageProps = {
   params: Promise<{
-    id:string
-  }>
-}
+    id: string;
+  }>;
+};
 export default async function AdminPostIdPage({
   params,
+}: AdminPostIdPageProps) {
+  const { id } = await params;
+  const post = await findPostByIdAdmin(id).catch();
 
-}:AdminPostIdPageProps) {
-  const {id} = await params
-  return <div className='py-16 text-4xl'>AdminPostIdPage {id}</div>;
+  if (!post) notFound();
+  const publicPost = makePublicpost(post);
+
+  return (
+    <div className='flex flex-col gap-6'>
+      <h1 className='text-xl font-extrabold'>Editar post</h1>
+      <ManagePostForm  publicPost={publicPost}/>
+    </div>
+  );
 }
--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -5,9 +5,14 @@ import { InputText } from '@/components/InputText/InputText';
 import { MarkdownEditor } from '@/components/MarkdownEditor';
 import { useState } from 'react';
 import { ImageUploader } from '../ImageUploader';
+import { PublicPost } from '@/dto/post/dto';
 
-export function ManagePostForm() {
-  const [contentValue, setContentValue] = useState('');
+type ManagePostFOrmProps = {
+  publicPost?: PublicPost;
+};
+
+export function ManagePostForm({ publicPost }: ManagePostFOrmProps) {
+  const [contentValue, setContentValue] = useState(publicPost?.content || '');
   return (
     <form action='' className='mb-16'>
       <div className='flex flex-col gap-6'>
@@ -17,7 +22,7 @@ export function ManagePostForm() {
           placeholder='ID gerado automaticamente'
           type='text'
           readOnly
-          defaultValue={''}
+          defaultValue={publicPost?.id || ''}
         />
 
         <InputText
@@ -26,7 +31,7 @@ export function ManagePostForm() {
           placeholder='Slug gerado automaticamente'
           type='text'
           readOnly
-          defaultValue={''}
+          defaultValue={publicPost?.slug || ''}
         />
         <InputText
           labelText='Autor'
@@ -34,7 +39,7 @@ export function ManagePostForm() {
           placeholder='digite o nome do autor'
           type='text'
           readOnly
-          defaultValue={''}
+          defaultValue={publicPost?.author || ''}
         />
 
         <InputText
@@ -42,14 +47,14 @@ export function ManagePostForm() {
           name='title'
           placeholder='Digite o titulo'
           type='text'
-          defaultValue={''}
+          defaultValue={publicPost?.title || ''}
         />
         <InputText
           labelText='Excerto'
           name='excerpt'
           placeholder='Digite o resumo'
           type='text'
-          defaultValue={''}
+          defaultValue={publicPost?.excerpt || ''}
         />
         <MarkdownEditor
           labelText='Conteudo'
@@ -64,13 +69,14 @@ export function ManagePostForm() {
           name='coverImageUrl'
           placeholder='Digite o URL da imagem'
           type='text'
-          defaultValue={''}
+          defaultValue={publicPost?.coverImageUrl || ''}
         />
 
         <InputCheckbox
           name='published'
           type='checkbox'
           labelText='Publicar?'
+          defaultChecked={publicPost?.published || false}
         ></InputCheckbox>
 
         <div className='mt-4'>

0d0a1169f2f211c6652ab2bb83f8a41f68a5666c
rfreitas16 <rfreitas16@hotmail.com>
2025-09-29 13:56:03 -0300

    gerando senhas hash com Bcrypt e base64

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -10,6 +10,19 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
   await asyncDelay(5000); // Vou manter
 
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados Invalidos',
+    };
+  }
+ //Dados que o usuario digitou no formulario
+  const username = formData.get('username')?.toString() || '';
+  const password = formData.get('password')?.toString() || '';
+
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = '';
+
   return {
     username: '',
     error: 'Teste de erro',

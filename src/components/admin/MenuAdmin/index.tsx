36e8e28fe9144e7d04d8cc84c7342d6f9b5235bf
rfreitas16 <rfreitas16@hotmail.com>
2025-10-02 14:46:56 -0300

    fazendo o logout

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,13 +1,23 @@
 'use client';
+import { logoutAction } from '@/actions/login/logout';
 import clsx from 'clsx';
-import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon, PlusIcon } from 'lucide-react';
+import {
+  CircleXIcon,
+  FileTextIcon,
+  HourglassIcon,
+  HouseIcon,
+  LogOutIcon,
+  MenuIcon,
+  PlusIcon,
+} from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { MouseEvent, useEffect, useState, useTransition } from 'react';
 
 export function MenuAdmin() {
   const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     setIsOpen(false);
@@ -35,6 +45,15 @@ export function MenuAdmin() {
     linkClasses,
     'text-blue-200 italic, sm:hidden',
   );
+
+  function handleLogout(
+    e: React.MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>,
+  ) {
+    e.preventDefault();
+    startTransition(async () => {
+      await logoutAction();
+    });
+  }
   return (
     <nav className={navClasses}>
       <button
@@ -63,9 +82,24 @@ export function MenuAdmin() {
       <Link className={linkClasses} href='/admin/post'>
         <FileTextIcon></FileTextIcon>Posts
       </Link>
-            <Link className={linkClasses} href='/admin/post/new'>
+      <Link className={linkClasses} href='/admin/post/new'>
         <PlusIcon></PlusIcon>Criar post
       </Link>
+
+      <a onClick={handleLogout} href='#' className={linkClasses}>
+        {isPending && (
+          <>
+            <HourglassIcon />
+            Aguarde...
+          </>
+        )}
+        {!isPending && (
+          <>
+            <LogOutIcon />
+            Sair
+          </>
+        )}
+      </a>
     </nav>
   );
 }
